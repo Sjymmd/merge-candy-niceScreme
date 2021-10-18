2875a62ca8b2d56bd15a842ec6c9ffb3d3486cd9
majc Pro <majc@fengeini.com>
2021-10-18 18:07:24 +0800

    update merge

--- src/App.tsx
+++ src/App.tsx
@@ -1,105 +1,62 @@
+import React, { Component } from "react";
+import ReactGA from "react-ga";
+import $ from "jquery";
 import "./App.css";
-import { useMemo } from "react";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Portfolio from "./Components/Portfolio";
 
-import Home from "./Home";
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            foo: "bar",
+            resumeData: {}
+        };
 
-import * as anchor from "@project-serum/anchor";
-import { clusterApiUrl } from "@solana/web3.js";
-import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
-import {
-  getPhantomWallet,
-  getSlopeWallet,
-  getSolflareWallet,
-  getSolletWallet,
-  getSolletExtensionWallet,
-} from "@solana/wallet-adapter-wallets";
+        ReactGA.initialize("UA-110570651-1");
+        ReactGA.pageview(window.location.pathname);
+    }
 
-import {
-  ConnectionProvider,
-  WalletProvider,
-} from "@solana/wallet-adapter-react";
+    getResumeData() {
+        $.ajax({
+            url: "./resumeData.json",
+            dataType: "json",
+            cache: false,
+            success: function(data) {
+                this.setState({ resumeData: data });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.log(err);
+                alert(err);
+            }
+        });
+    }
 
-import { WalletDialogProvider } from "@solana/wallet-adapter-material-ui";
-import { createTheme, ThemeProvider } from "@material-ui/core";
+    componentDidMount() {
+        this.getResumeData();
+    }
 
-const treasury = new anchor.web3.PublicKey(
-  process.env.REACT_APP_TREASURY_ADDRESS!
-);
-
-const config = new anchor.web3.PublicKey(
-  process.env.REACT_APP_CANDY_MACHINE_CONFIG!
-);
-
-const candyMachineId = new anchor.web3.PublicKey(
-  process.env.REACT_APP_CANDY_MACHINE_ID!
-);
-
-const network = process.env.REACT_APP_SOLANA_NETWORK as WalletAdapterNetwork;
-
-const rpcHost = process.env.REACT_APP_SOLANA_RPC_HOST!;
-const connection = new anchor.web3.Connection(rpcHost);
-
-const startDateSeed = parseInt(process.env.REACT_APP_CANDY_START_DATE!, 10);
-
-const txTimeout = 30000; // milliseconds (confirm this works for your project)
-
-const theme = createTheme({
-    palette: {
-        type: 'dark',
-    },
-    overrides: {
-        MuiButtonBase: {
-            root: {
-                justifyContent: 'flex-start',
-            },
-        },
-        MuiButton: {
-            root: {
-                textTransform: undefined,
-                padding: '12px 16px',
-            },
-            startIcon: {
-                marginRight: 8,
-            },
-            endIcon: {
-                marginLeft: 8,
-            },
-        },
-    },
-});
-
-const App = () => {
-  const endpoint = useMemo(() => clusterApiUrl(network), []);
-
-  const wallets = useMemo(
-    () => [
-        getPhantomWallet(),
-        getSlopeWallet(),
-        getSolflareWallet(),
-        getSolletWallet({ network }),
-        getSolletExtensionWallet({ network })
-    ],
-    []
-  );
-
-  return (
-      <ThemeProvider theme={theme}>
-        <ConnectionProvider endpoint={endpoint}>
-          <WalletProvider wallets={wallets} autoConnect={true}>
-            <WalletDialogProvider>
-              <Home
-                candyMachineId={candyMachineId}
-                config={config}
-                connection={connection}
-                startDate={startDateSeed}
-                treasury={treasury}
-                txTimeout={txTimeout}
-              />
-            </WalletDialogProvider>
-          </WalletProvider>
-        </ConnectionProvider>
-      </ThemeProvider>
-  );
-};
+    render() {
+        return ( <
+            div className = "App" >
+            <
+            Header data = { this.state.resumeData.main }
+            /> <
+            About data = { this.state.resumeData.main }
+            /> <
+            Resume data = { this.state.resumeData.resume }
+            /> <
+            Portfolio data = { this.state.resumeData.portfolio }
+            /> <
+            Footer data = { this.state.resumeData.main }
+            /> <
+            /div>
+        );
+    }
+}
 
 export default App;
